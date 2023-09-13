14922d795c194941846d241fc7743716a60dcada
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-09-13 12:06:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def list_instances():
     
     return render_template('list_instances.html', instances=instance_data)
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
 
