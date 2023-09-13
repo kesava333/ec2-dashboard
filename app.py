8c0ec6c8e7785547a5c426402852cc0dc9c88b80
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-09-13 12:09:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
 @app.route('/ec2', methods=['POST'])
 def ec2():
     ec2 = boto3.client('ec2')
-    instance_id = request.form.get('instance_id')
+    instance_id = request.form.get('i-07ee73a38da3f775a')
     action = request.form.get('action')
     if action == 'start':
         ec2.start_instances(InstanceIds=[instance_id])
