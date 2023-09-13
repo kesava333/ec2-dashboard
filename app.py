ea6a44bd001f88085138de7faeb7021f4e041102
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-09-13 11:32:26 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import boto3
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/ec2', methods=['POST'])
+def ec2():
+    ec2 = boto3.client('ec2')
+    instance_id = request.form.get('instance_id')
+    action = request.form.get('action')
+    if action == 'start':
+        ec2.start_instances(InstanceIds=[instance_id])
+    elif action == 'stop':
+        ec2.stop_instances(InstanceIds=[instance_id])
+    elif action == 'reboot':
+        ec2.reboot_instances(InstanceIds=[instance_id])
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
