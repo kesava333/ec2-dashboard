2d376caaaedfe37fdd0033ac534274a3eb9cdc1c
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-09-13 12:49:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,37 @@ def list_ec2_instances():
             instance_list.append(instance_details)
 
     return render_template('ec2_instances.html', instances=instance_list)
+    
+@app.route('/start_instance', methods=['POST'])
+def start_instance():
+    instance_id = request.form.get('instance_id')
+    try:
+        ec2.start_instances(InstanceIds=[instance_id])
+        flash(f"Starting EC2 instance {instance_id}", 'success')
+    except Exception as e:
+        flash(f"Failed to start EC2 instance {instance_id}: {str(e)}", 'error')
+    return redirect('/')
 
+@app.route('/stop_instance', methods=['POST'])
+def stop_instance():
+    instance_id = request.form.get('instance_id')
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id])
+        flash(f"Stopping EC2 instance {instance_id}", 'success')
+    except Exception as e:
+        flash(f"Failed to stop EC2 instance {instance_id}: {str(e)}", 'error')
+    return redirect('/')
+
+@app.route('/terminate_instance', methods=['POST'])
+def terminate_instance():
+    instance_id = request.form.get('instance_id')
+    try:
+        ec2.terminate_instances(InstanceIds=[instance_id])
+        flash(f"Terminating EC2 instance {instance_id}", 'success')
+    except Exception as e:
+        flash(f"Failed to terminate EC2 instance {instance_id}: {str(e)}", 'error')
+    return redirect('/')
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
