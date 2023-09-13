f50ba0985df3ef0f32b00d070e987e50e5eb3e2f
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-09-13 12:30:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,30 @@
 import boto3
-from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/ec2', methods=['POST'])
-def ec2():
-    ec2 = boto3.client('ec2')
-    action = request.form.get('action')
-    # Retrieve a list of all EC2 instances
-    instances = ec2.describe_instances()
-    
-    # Extract and format instance information
-    for reservation in instances['Reservations']:
-        for instance in reservation['Instances']:
-            instance_id = instance['InstanceId']
-            if action == 'start':
-                ec2.start_instances(InstanceIds=[instance_id])
-            elif action == 'stop':
-                ec2.stop_instances(InstanceIds=[instance_id])
-            elif action == 'reboot':
-                ec2.reboot_instances(InstanceIds=[instance_id])
-    
-    return render_template('index.html')
-@app.route('/list_instances', methods=['GET'])
-def list_instances():
-    ec2 = boto3.client('ec2')
-    # Retrieve a list of all EC2 instances
-    instances = ec2.describe_instances()
-    # Extract and format instance information
-    instance_data = []
-    for reservation in instances['Reservations']:
-        for instance in reservation['Instances']:
-            instance_id = instance['InstanceId']
-            instance_state = instance['State']['Name']
-            instance_data.append({'InstanceID': instance_id, 'State': instance_state})
-    
-    return render_template('list_instances.html', instances=instance_data)
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+from flask import Flask, render_template
 
+app = Flask(__name__)
+# Initialize the Boto3 EC2 client
+ec2 = boto3.client('ec2')
+
+@app.route('/')
+def list_ec2_instances():
+    # Retrieve a list of EC2 instances
+    instances = ec2.describe_instances()
+
+    # Extract instance details
+    instance_list = []
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_details = {
+                'InstanceID': instance['InstanceId'],
+                'InstanceType': instance['InstanceType'],
+                'State': instance['State']['Name'],
+                'PublicDNS': instance.get('PublicDnsName', 'N/A'),
+                'PrivateIP': instance['PrivateIpAddress']
+            }
+            instance_list.append(instance_details)
+
+    return render_template('ec2_instances.html', instances=instance_list)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
